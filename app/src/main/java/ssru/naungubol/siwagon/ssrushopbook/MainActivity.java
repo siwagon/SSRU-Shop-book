732d91ac929bedc159020a8a92a6a39491e1b2e3
siwagon <siwagon1@gmail.com>
2016-05-31 15:38:58 +0700

    Alert Login

--- app/src/main/java/ssru/naungubol/siwagon/ssrushopbook/MainActivity.java
+++ app/src/main/java/ssru/naungubol/siwagon/ssrushopbook/MainActivity.java
@@ -1,6 +1,7 @@
 package ssru.naungubol.siwagon.ssrushopbook;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String urlJSON = "http://swiftcodingthai.com/ssru/get_user_siwagon.php";
     private EditText userEdittext, passwordEditText;
     private String userString, passwordString;
+    private String[] loginStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,9 +61,51 @@ public class MainActivity extends AppCompatActivity {
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องจ๊ะ");
 
+        } else {
+            checkUserAndPassword();
         }
 
     }   // clickSignIn
+
+    private void checkUserAndPassword() {
+
+        try {
+
+            SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                    MODE_PRIVATE, null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM userTABLE WHERE User = " + "'" + userString + "'", null);
+            cursor.moveToFirst();
+
+            loginStrings = new String[cursor.getColumnCount()];
+
+            for (int i=0;i<cursor.getColumnCount();i++) {
+
+                loginStrings[i] = cursor.getString(i);
+
+            }   // for
+            cursor.close();
+
+            //Check Password
+            if (passwordString.equals(loginStrings[4])) {
+                //Password True
+                Toast.makeText(this, "ยินดีต้อนรับ " + loginStrings[1] + " " + loginStrings[2],
+                        Toast.LENGTH_SHORT).show();
+
+            } else {
+                //Password False
+                MyAlert myAlert = new MyAlert();
+                myAlert.myDialog(this, "รหัสผ่านผิด", "กรุณากรอกรหัสผ่านใหม่อีกครั้ง");
+            }
+
+
+        } catch (Exception e) {
+
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "ไม่มี User นี้", "ไม่มี " + userString + "ในฐานข้อมูลของเรา");
+
+        }
+
+    }
 
 
     private void synJSONtoSQLite() {
